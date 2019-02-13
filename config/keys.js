97a53d8f7c1e0b8ba738914b708ba9b95304fc64
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-13 07:06:57 +0530

    connect mongodb using mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://tharindu123:mttharindu@tharindu1-shard-00-00-fahby.mongodb.net:27017,tharindu1-shard-00-01-fahby.mongodb.net:27017,tharindu1-shard-00-02-fahby.mongodb.net:27017/test?ssl=true&replicaSet=tharindu1-shard-0&authSource=admin&retryWrites=true"
+};
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hello rrtharindu world"));
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDb connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("hello tharindu world"));
 
 const port = process.env.PORT || 5000;
 
