e4a4e9e24d7fc2330c303df24e3318900057faa9
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-16 23:51:05 +0530

    sign jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 //load User model
 const User = require("../../models/User");
@@ -43,6 +45,46 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route GET api/users/login
+// @desc Login user/ returining JWT Token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not find" });
+    }
+
+    //ckeck pasword
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; //create jwt payload
+
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer" + token
+            });
+          }
+        );
+      } else {
+        return res.status(404).json({ password: "password incorrect" });
+      }
+    });
   });
 });
 
