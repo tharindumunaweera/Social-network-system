5678a64b0b02c7004d5a15c4b84ebfa682af505e
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-16 16:24:43 +0530

    use gravatar for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
 
+//load User model
+const User = require("../../models/User");
+
+// @route GET api/users/test
+// @desc Tests users route
+// @access Public
 router.get("/test", (req, res) => res.json({ msg: "users work" }));
+
+// @route GET api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
