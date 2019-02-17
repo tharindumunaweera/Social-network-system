bb7ff5cdf734105a4f3c3e901439f2c255f50213
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-17 16:38:28 +0530

    register form name validtion

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 //load User model
 const User = require("../../models/User");
 
@@ -18,6 +21,12 @@ router.get("/test", (req, res) => res.json({ msg: "users work" }));
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
