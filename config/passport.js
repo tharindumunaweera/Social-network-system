beaf84efa2d0d04af939863f6932557d4d020271
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-18 11:10:39 +0530

    create a profile model

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("../config/keys");
 
-/*const opts = {};
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
@@ -22,8 +22,8 @@ module.exports = passport => {
     })
   );
 };
-var JwtStrategy = require("passport-jwt").Strategy,
-  ExtractJwt = require("passport-jwt").ExtractJwt;*/
+/*var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = "secret";
@@ -45,4 +45,4 @@ module.exports = passport => {
       });
     })
   );
-};
+};*/
