b891fb3bf459353328c719e7014c232d2f4f8a1a
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-02-24 07:33:25 +0530

    create add experience post requesta nd validate

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 
 // Load Validation
 const validaterProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
 
 //Load profile Model
 const Profile = require("../../models/Profile");
@@ -157,6 +158,39 @@ router.post(
           new Profile(profileFields).save().then(profile => res.json(profile));
         });
       }
+    });
+  }
+);
+
+// @route POST api/profile/experience
+// @desc Add experience to the profile
+// @access Private
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      //Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
     });
   }
 );
